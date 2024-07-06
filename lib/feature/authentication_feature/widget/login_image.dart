f1838f6c211ef569e791a822c291b77562b2a221
user <mahmoud.enshagshan0@gmail.com>
2024-07-06 12:48:02 +0300

    1- create login_image

---
+++ lib/feature/authentication_feature/widget/login_image.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginImage extends StatelessWidget {
+  const LoginImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.only(
+          left: MediaQuery.of(context).size.width * 0.16,
+          right: MediaQuery.of(context).size.width * 0.16,
+          top: MediaQuery.of(context).size.width * 0.08,
+        ),
+        child: Image.asset('assets/images/img.png',
+        width: 255,
+          height: 255,
+        ),
+      ),
+    );
+  }
+}
--- lib/feature/authentication_feature/widget/login_view_body.dart
+++ lib/feature/authentication_feature/widget/login_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kafiil_test/feature/authentication_feature/widget/custom_appbar_widget.dart';
+import 'package:kafiil_test/feature/authentication_feature/widget/login_image.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
@@ -9,8 +10,10 @@ class LoginViewBody extends StatelessWidget {
     return const SafeArea(
       child: Scaffold(
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            CustomAppbarWidget(titleName: 'Account Login')
+            CustomAppbarWidget(titleName: 'Account Login',),
+            LoginImage()
           ],
         ),
       ),
